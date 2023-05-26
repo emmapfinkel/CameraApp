abf1bcfee230688617fee1994ad8607f98cb9e9d
Alisona <alisonahle@gmail.com>
2023-05-26 11:09:02 -0400

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by scholar on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
